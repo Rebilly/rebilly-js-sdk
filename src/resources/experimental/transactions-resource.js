6992a2a4ca9dc6bf30fe9c2fa27afa1e6ca14d32
gikgit <hejin.udem@gmail.com>
2017-11-17 12:18:47 -0500

    set default id = ''

--- src/resources/experimental/transactions-resource.js
+++ src/resources/experimental/transactions-resource.js
@@ -1,6 +1,6 @@
 export default function TransactionsResource({apiHandler}) {
     return {
-        async reschedule({id, data}) {
+        async reschedule({id = '', data}) {
             return await apiHandler.post(`/experimental/transactions/${id}/reschedule`, data);
         }
     };
