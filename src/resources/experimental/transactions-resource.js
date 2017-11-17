b59729974c41e9566e9a766551be1de4a3fe8eac
gikgit <hejin.udem@gmail.com>
2017-11-17 16:27:53 -0500

    fix endpoint error

--- src/resources/experimental/transactions-resource.js
+++ src/resources/experimental/transactions-resource.js
@@ -1,7 +1,7 @@
 export default function TransactionsResource({apiHandler}) {
     return {
         async reschedule({id = '', data}) {
-            return await apiHandler.post(`/experimental/transactions/${id}/reschedule`, data);
+            return await apiHandler.post(`transactions/${id}/reschedule`, data);
         }
     };
 };
