371f01a953e1eacf37f446c6b111bc2b42695fec
Olivier <oliviertr@users.noreply.github.com>
2018-01-10 14:18:25 -0500

    Add export-schedules resource to SDK

---
+++ src/resources/experimental/export-schedules-resource.js
@@ -0,0 +1,24 @@
+export default function ExportSchedulesResource({apiHandler}) {
+    return {
+        async getAll({resource, limit = null, offset = null, sort = null, expand = null, filter = null, q = null, criteria = null} = {}) {
+            const params = {
+                limit,
+                offset,
+                sort,
+                expand,
+                filter,
+                q,
+                criteria
+            };
+            return await apiHandler.getAll(`export-schedules/${resource}`, params);
+        },
+
+        async get({resource, id}) {
+            return await apiHandler.get(`export-schedules/${resource}/${id}`);
+        },
+
+        async create({resource, data}) {
+            return await apiHandler.post(`export-schedules/${resource}`, data);
+        }
+    };
+};
