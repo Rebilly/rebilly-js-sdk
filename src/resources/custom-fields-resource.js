d97e9d18f08f96636fd8ef8b1c4cbd7316d64709
Demi <2e3s19@gmail.com>
2018-04-30 19:01:20 -0400

    Remove custom fields deletion

--- src/resources/custom-fields-resource.js
+++ src/resources/custom-fields-resource.js
@@ -19,9 +19,5 @@ export default function CustomFieldsResource({apiHandler}) {
         async update({resource, name, data}) {
             return await apiHandler.put(`custom-fields/${resource}/${name}`, data);
         },
-
-        async delete({resource, name}) {
-            return await apiHandler.delete(`custom-fields/${resource}/${name}`);
-        }
     };
 };
