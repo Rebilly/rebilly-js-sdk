3ea8e629649888a6efe9de87bebeb2827428dca8
Tulio Diaz <t_diaz@ad.ilabsinc.com>
2018-06-21 17:03:29 -0400

    Saved changes

--- src/resources/gateway-accounts-resource.js
+++ src/resources/gateway-accounts-resource.js
@@ -35,5 +35,29 @@ export default function GatewayAccountsResource({apiHandler}) {
         async disable({id}) {
             return await apiHandler.post(`gateway-accounts/${id}/disable`);
         },
+
+        async getAllDowntimeSchedules({id, limit = null, offset = null} = {}) {
+            const params = {
+                limit,
+                offset,
+            };
+            return await apiHandler.getAll(`gateway-accounts/${id}/downtime-schedules`, params);
+        },
+
+        async getDowntimeSchedule({id, downtimeScheduleId}) {
+            return await apiHandler.get(`gateway-accounts/${id}/downtime-schedules/${downtimeScheduleId}`);
+        },
+
+        async createDowntimeSchedule({id, data}) {
+            return await apiHandler.put(`gateway-accounts/${id}/downtime-schedules`, data);
+        },
+
+        async updateDowntimeSchedule({id, downtimeScheduleId, data}) {
+            return await apiHandler.put(`gateway-accounts/${id}/downtime-schedules/${downtimeScheduleId}`, data);
+        },
+
+        async deleteDowntimeSchedule({id, downtimeScheduleId}) {
+            return await apiHandler.delete(`gateway-accounts/${id}/downtime-schedules/${downtimeScheduleId}`);
+        }
     };
 };
