19829bb5d1ca82119a926056e9c48a5c63ad64fb
Tulio <tuliodiaz126@gmail.com>
2018-06-27 14:04:18 -0400

    Add gateway downtime schedule (#77)
    Added nested resource DowntimeSchedules into GatewayAccounts

--- src/resources/gateway-accounts-resource.js
+++ src/resources/gateway-accounts-resource.js
@@ -35,5 +35,31 @@ export default function GatewayAccountsResource({apiHandler}) {
         async disable({id}) {
             return await apiHandler.post(`gateway-accounts/${id}/disable`);
         },
+
+        async getAllDowntimeSchedules({id, limit = null, offset = null, filter = null} = {}) {
+            const params = {
+                limit,
+                offset,
+                filter,
+            };
+            return await apiHandler.getAll(`gateway-accounts/${id}/downtime-schedules`, params);
+        },
+
+        async getDowntimeSchedule({id, downtimeScheduleId}) {
+            return await apiHandler.get(`gateway-accounts/${id}/downtime-schedules/${downtimeScheduleId}`);
+        },
+
+        async createDowntimeSchedule({id, data}) {
+            // Id for a downtime-schedules cannot be set, is read only.
+            return await apiHandler.create(`gateway-accounts/${id}/downtime-schedules`, '', data);
+        },
+
+        async updateDowntimeSchedule({id, downtimeScheduleId, data}) {
+            return await apiHandler.put(`gateway-accounts/${id}/downtime-schedules/${downtimeScheduleId}`, data);
+        },
+
+        async deleteDowntimeSchedule({id, downtimeScheduleId}) {
+            return await apiHandler.delete(`gateway-accounts/${id}/downtime-schedules/${downtimeScheduleId}`);
+        }
     };
 };
