ca628a872f709023071c7b7953d0605451983bd0
Andrei Moldoveanu <andrei@ilabsinc.com>
2018-07-17 11:13:45 -0400

    Add support for Mailgun credentials

--- src/resources/credential-hashes-resource.js
+++ src/resources/credential-hashes-resource.js
@@ -8,12 +8,20 @@ export default function CredentialHashesResource({apiHandler}) {
             return await apiHandler.get(`credential-hashes/webhooks/${hash}`);
         },
 
+        async getMailgunCredential({hash}) {
+            return await apiHandler.get(`credential-hashes/mailgun/${hash}`);
+        },
+
         async createEmailCredential({data}) {
             return await apiHandler.post(`credential-hashes/emails`, data);
         },
 
         async createWebhookCredential({data}) {
             return await apiHandler.post(`credential-hashes/webhooks`, data);
+        },
+
+        async createMailgunCredential({data}) {
+            return await apiHandler.post(`credential-hashes/mailgun`, data);
         }
     };
 };
