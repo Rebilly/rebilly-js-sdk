1390f891ecef37220a2b05de0aa349416a494aeb
Natalya Semenova <natalya.semenova@rebilly.com>
2019-02-06 19:39:45 +0300

    style is unified

--- src/resources/tags-resource.js
+++ src/resources/tags-resource.js
@@ -3,44 +3,56 @@ const RESOURCE = 'tags';
 
 export default function TagsResource({apiHandler}) {
     return {
-        getAll: async ({
+        async getAll({
             limit = null,
             offset = null,
             filter = null,
             q = null,
             sort = null,
-        } = {}) => apiHandler.getAll(RESOURCE, {
-            limit,
-            offset,
-            filter,
-            q,
-            sort,
-        }),
+        } = {}) {
+            return apiHandler.getAll(RESOURCE, {
+                limit,
+                offset,
+                filter,
+                q,
+                sort,
+            });
+        },
 
-        create: async ({name}) => apiHandler.post(`${RESOURCE}/`, {name}),
+        async create({name}) {
+            return apiHandler.post(`${RESOURCE}/`, {name});
+        },
 
-        get: async ({tag}) => apiHandler.get(`${RESOURCE}/${tag}`),
+        async get ({tag}) {
+            return apiHandler.get(`${RESOURCE}/${tag}`);
+        },
 
         /**
          * @param tag {String} The tag name
          * @param name {String} New unique tag name
          */
-        update: async ({tag, name}) => apiHandler.put(`${RESOURCE}/${tag}`, {name}),
+        async update ({tag, name}) {
+            return apiHandler.put(`${RESOURCE}/${tag}`, {name});
+        },
 
-        delete: async ({tag}) => apiHandler.delete(`${RESOURCE}/${tag}`),
+        async delete ({tag}) {
+            return apiHandler.delete(`${RESOURCE}/${tag}`);
+        },
 
-        tagCustomers: async ({tag, ids}) => apiHandler.post(
-            `${RESOURCE}/${tag}/customers`,
-            {customerIds: ids},
-        ),
+        async tagCustomers ({tag, ids}) {
+            return apiHandler.post(`${RESOURCE}/${tag}/customers`, {customerIds: ids});
+        },
 
-        tagCustomer: async ({tag, id}) => apiHandler.post(`${RESOURCE}/${tag}/customers/${id}`),
+        async tagCustomer ({tag, id}) {
+            return apiHandler.post(`${RESOURCE}/${tag}/customers/${id}`);
+        },
 
-        untagCustomers: async ({tag, ids}) => apiHandler.deleteAll(
-            `${RESOURCE}/${tag}/customers`,
-            {customerIds: ids},
-        ),
+        async untagCustomers ({tag, ids}) {
+            return apiHandler.deleteAll(`${RESOURCE}/${tag}/customers`, {customerIds: ids});
+        },
 
-        untagCustomer: async ({tag, id}) => apiHandler.delete(`${RESOURCE}/${tag}/customers/${id}`),
+        async untagCustomer ({tag, id}) {
+            return apiHandler.delete(`${RESOURCE}/${tag}/customers/${id}`);
+        },
     };
 };
