8d3f11bc5a9a59be1abad5e50d1eade754eef6d6
Veaceslav Medvedev <slavcopost@gmail.com>
2019-05-28 15:46:13 +0200

    Add get integration method

--- src/resources/integrations-resource.js
+++ src/resources/integrations-resource.js
@@ -2,6 +2,10 @@ export default function IntegrationsResource({apiHandler}) {
     return {
         getAll(params) {
             return apiHandler.getAll('integrations', params);
-        }
+        },
+
+        get({label, expand = null}) {
+            return apiHandler.get(`integrations/${label}`, {expand});
+        },
     };
 };
