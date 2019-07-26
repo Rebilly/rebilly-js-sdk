3d3e7679e7d45caf54dff2425f510b2e615a1179
Mathew Sonke <mathew.sonke@gmail.com>
2019-07-26 12:00:35 -0400

    remove dead methods

--- src/resources/email-notifications-resource.js
+++ src/resources/email-notifications-resource.js
@@ -7,34 +7,5 @@ export default function EmailNotificationsResource({apiHandler}) {
         getAll() {
             return apiHandler.getAll('email-notifications');
         },
-
-        /**
-         * Retrieve a email notification
-         * @param id
-         * @returns {Promise}
-         */
-        get({id}) {
-            return apiHandler.get(`email-notifications/${id}`);
-        },
-
-        /**
-         * Create a email notification
-         * @param id
-         * @param data
-         * @returns {Promise}
-         */
-        create({id = '', data}) {
-            return apiHandler.create(`email-notifications/${id}`, id, data);
-        },
-
-        /**
-         * Update a email notification
-         * @param id
-         * @param data
-         * @returns {Promise}
-         */
-        update({id, data}) {
-            return apiHandler.put(`email-notifications/${id}`, data);
-        },
     };
 };
