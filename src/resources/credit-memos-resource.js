cd3332a27fd12af62295fbd29bb38cc2e3d5f721
Marin Bezhanov <marin.bezhanov@gmail.com>
2019-08-06 14:44:28 +0300

    Add timeline endpoints to Credit Memo resource

--- src/resources/credit-memos-resource.js
+++ src/resources/credit-memos-resource.js
@@ -21,5 +21,27 @@ export default function CreditMemosResource({apiHandler}) {
         getAllTransactions({id}) {
             return apiHandler.getAll(`credit-memos/${id}/transactions`);
         },
+
+        getAllTimelineMessages({id, limit = null, offset = null, sort = null, filter = null} = {}) {
+            const params = {
+                limit,
+                offset,
+                sort,
+                filter,
+            };
+            return apiHandler.getAll(`credit-memos/${id}/timeline`, params);
+        },
+
+        getTimelineMessage({id, messageId = ''} = {}) {
+            return apiHandler.get(`credit-memos/${id}/timeline/${messageId}`);
+        },
+
+        deleteTimelineMessage({id, messageId}) {
+            return apiHandler.delete(`credit-memos/${id}/timeline/${messageId}`);
+        },
+
+        createTimelineComment({id, data}) {
+            return apiHandler.create(`credit-memos/${id}/timeline`, '', data);
+        },
     };
 };
