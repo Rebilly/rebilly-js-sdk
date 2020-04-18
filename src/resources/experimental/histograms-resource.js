0715be8a52613e030c06657e9f72c8b161e6e936
Daria Baklanova <darya.baklanova@gmail.com>
2020-04-18 21:02:28 +0700

    support filter param for histograms API request

--- src/resources/experimental/histograms-resource.js
+++ src/resources/experimental/histograms-resource.js
@@ -1,12 +1,13 @@
 export default function HistogramsResource({apiHandler}) {
     return {
-        getTransactionHistogramReport({periodStart = '', periodEnd = '', aggregationField = '', aggregationPeriod = '', metric = '', criteria = null} = {}) {
+        getTransactionHistogramReport({periodStart = '', periodEnd = '', aggregationField = '', aggregationPeriod = '', metric = '', filter = '', criteria = null} = {}) {
             const params = {
                 periodStart,
                 periodEnd,
                 aggregationField,
                 aggregationPeriod,
                 metric,
+                filter,
                 criteria,
             };
             return apiHandler.get(`histograms/transactions`, params);
