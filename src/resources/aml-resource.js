549fbc684cdb7ff425ea4a8944c2894d615d3f44
Mike Classic <mclassic@users.noreply.github.com>
2020-05-25 08:31:38 -0400

    Add country to AML API (#208)

--- src/resources/aml-resource.js
+++ src/resources/aml-resource.js
@@ -1,9 +1,13 @@
 export default function AmlResource({apiHandler}) {
     return {
-        getAll({firstName, lastName, dob = null}) {
+        getAll({firstName, lastName, dob = null, country = null}) {
             let params = {firstName, lastName};
             if (dob) {
                 params = {...params, dob};
+            }
+
+            if (country) {
+                params = {...params, country};
             }
 
             return apiHandler.getAll('aml', params);
