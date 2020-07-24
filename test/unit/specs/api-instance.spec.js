f5a706d8dfe1da90e7f7d782f0691d0184af8d37
Cesar Level <cesarlevel@gmail.com>
2020-07-24 15:45:10 -0400

    Add checkout forms resource (#219)

--- test/unit/specs/api-instance.spec.js
+++ test/unit/specs/api-instance.spec.js
@@ -24,6 +24,7 @@ describe('when I create an API instance', () => {
         expect(apiInstance.bankAccounts).to.be.an('object');
         expect(apiInstance.blacklists).to.be.an('object');
         expect(apiInstance.checkoutPages).to.be.an('object');
+        expect(apiInstance.checkoutForms).to.be.an('object');
         expect(apiInstance.coupons).to.be.an('object');
         expect(apiInstance.customers).to.be.an('object');
         expect(apiInstance.customerAuthentication).to.be.an('object');
