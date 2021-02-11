14131d1742d2e85015a8642888bf374b5843a05d
Alberto Gualis <agualis@gmail.com>
2021-02-11 09:36:50 +0100

    Rename typings to types

--- typings/rebilly/generated-types.d.ts
+++
@@ -1,4 +0,0 @@
-
-    /*GENERATED TYPES: DON'T MANUALLY UPDATE THIS FILE*/
-    export type PostSignupResponse = Promise<{fields: operations['PostSignupRequest']['responses'][201]['application/json']}>
-
