3976523788ba393e510f25e541ac6bdeba0f08e7
Alberto Gualis <agualis@gmail.com>
2021-03-30 17:40:06 +0200

    avoid changes in blocklists

--- src/resources/blocklists-resource.js
+++ src/resources/blocklists-resource.js
@@ -1,20 +1,20 @@
 export default function BlocklistsResource({apiHandler}) {
     return {
-       getAll({limit = null, offset = null, sort = null, q = null, filter = null} = {}) {
-           const params = {
-               limit,
-               offset,
-               sort,
-               q,
-               filter
+        getAll({limit = null, offset = null, sort = null, q = null, filter = null} = {}) {
+            const params = {
+                limit,
+                offset,
+                sort,
+                q,
+                filter
             };
             return apiHandler.getAll(`blocklists`, params);
         },
-        
+
         get({id}) {
             return apiHandler.get(`blocklists/${id}`);
         },
-        
+
         create({id = '', data}) {
             return apiHandler.create(`blocklists/${id}`, id, data);
         },
