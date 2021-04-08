095cb3725470adbd9f91d2adddbb6e925590593c
Alberto Gualis <agualis@gmail.com>
2021-04-08 11:05:32 +0200

    Auto generate plans resource

--- src/resources/plans-resource.js
+++ src/resources/plans-resource.js
@@ -4,22 +4,29 @@
 */
 
 export default function PlansResource({apiHandler}) {
-    return {
-      getAll({limit = null, offset = null, expand = null} = {}) {
-        const params = {limit, offset, expand};
-        return apiHandler.getAll(`plans`, params);
-      },
-      create({id = '', data}) {
-        return apiHandler.create(`plans/${id}`, id, data);
-      },
-      get({id}) {
-        return apiHandler.get(`plans/${id}`);
-      },
-      update({id, data}) {
-        return apiHandler.put(`plans/${id}`, data);
-      },
-      delete({id}) {
-        return apiHandler.delete(`plans/${id}`);
-      },
-    };
-  }
+  return {
+    getAll({
+      filter = null,
+      sort = null,
+      limit = null,
+      offset = null,
+      q = null,
+      expand = null,
+    } = {}) {
+      const params = {filter, sort, limit, offset, q, expand};
+      return apiHandler.getAll(`plans`, params);
+    },
+    create({id = '', data}) {
+      return apiHandler.create(`plans/${id}`, id, data);
+    },
+    get({id}) {
+      return apiHandler.get(`plans/${id}`);
+    },
+    update({id, data}) {
+      return apiHandler.put(`plans/${id}`, data);
+    },
+    delete({id}) {
+      return apiHandler.delete(`plans/${id}`);
+    },
+  };
+}
