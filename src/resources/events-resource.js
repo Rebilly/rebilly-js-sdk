c384adbf984ed4b00f8b66a260565677ba40547c
Alberto Gualis <agualis@gmail.com>
2021-04-14 18:00:58 +0200

    Update auto-generated source

--- src/resources/events-resource.js
+++ src/resources/events-resource.js
@@ -4,94 +4,92 @@
 */
 
 export default function EventsResource({apiHandler}) {
-    return {
-      getAll({limit = null, offset = null} = {}) {
-          const params = {
-              limit,
-              offset
-          };
-          return apiHandler.getAll(`events`, params);
-      },
-      get({eventType}) {
-          return apiHandler.get(`events/${eventType}`);
-      },
-      getRules({eventType}) {
-        return apiHandler.get(`events/${eventType}/rules`);
-      },
-      updateRules({eventType, data}) {
-        return apiHandler.put(`events/${eventType}/rules`, data);
-      },
-      getAllTimelineMessages({
-        eventType = null,
-        limit = null,
-        offset = null,
-        filter = null,
-        expand = null,
-      } = {}) {
-        const params = {limit, offset, filter, expand};
-        return apiHandler.getAll(`events/${eventType}/timeline`, params);
-      },
-      createTimelineComment({eventType, data}) {
-        return apiHandler.post(`events/${eventType}/timeline`, data);
-      },
-      getTimelineMessage({eventType, messageId}) {
-        return apiHandler.get(`events/${eventType}/timeline/${messageId}`);
-      },
-      deleteTimelineMessage({eventType, messageId}) {
-        return apiHandler.delete(`events/${eventType}/timeline/${messageId}`);
-      },
-      getRulesHistory({
-        eventType = null,
-        limit = null,
-        offset = null,
-        filter = null,
-        q = null,
-        sort = null,
-        fields = null,
-        expand = null,
-      } = {}) {
-        const params = {limit, offset, filter, q, sort, fields, expand};
-        return apiHandler.getAll(`events/${eventType}/rules/history`, params);
-      },
-      getRulesVersionNumber({eventType, version, fields, expand = null}) {
-        const params = {fields, expand};
-        return apiHandler.get(
-          `events/${eventType}/rules/history/${version}`,
-          params
-        );
-      },
-      getRulesVersionDetail({eventType, version, fields, expand = null}) {
-        const params = {fields, expand};
-        return apiHandler.get(
-          `events/${eventType}/rules/versions/${version}`,
-          params
-        );
-      },
-      getAllDraftRuleSets({
-        eventType = null,
-        limit = null,
-        offset = null,
-        filter = null,
-        q = null,
-        sort = null,
-        fields = null,
-        expand = null,
-      } = {}) {
-        const params = {limit, offset, filter, q, sort, fields, expand};
-        return apiHandler.getAll(`events/${eventType}/rules/drafts`, params);
-      },
-      createDraftRuleSet({eventType, data}) {
-        return apiHandler.post(`events/${eventType}/rules/drafts`, data);
-      },
-      getAllDraftRuleSet({eventType, id, fields, expand = null}) {
-        const params = {fields, expand};
-        return apiHandler.get(`events/${eventType}/rules/drafts/${id}`, params);
-      },
-      updateDraftRuleSet({eventType, id, data}) {
-        return apiHandler.put(`events/${eventType}/rules/drafts/${id}`, data);
-      },
-      deleteDraftRuleSet({eventType, id}) {
-        return apiHandler.delete(`events/${eventType}/rules/drafts/${id}`);
-      },
-    };
-  }
+  return {
+    getAll() {
+      return apiHandler.getAll(`events`);
+    },
+    get({eventType}) {
+      return apiHandler.get(`events/${eventType}`);
+    },
+    getRules({eventType}) {
+      return apiHandler.get(`events/${eventType}/rules`);
+    },
+    createRules({eventType, data}) {
+      return apiHandler.put(`events/${eventType}/rules`, data);
+    },
+    updateRules({eventType, data}) {
+      return apiHandler.put(`events/${eventType}/rules`, data);
+    },
+    getAllTimelineMessages({
+      eventType,
+      limit = null,
+      offset = null,
+      filter = null,
+    }) {
+      const params = {limit, offset, filter};
+      return apiHandler.getAll(`events/${eventType}/timeline`, params);
+    },
+    createTimelineComment({eventType, data}) {
+      return apiHandler.post(`events/${eventType}/timeline`, data);
+    },
+    getTimelineMessage({eventType, messageId}) {
+      return apiHandler.get(`events/${eventType}/timeline/${messageId}`);
+    },
+    deleteTimelineMessage({eventType, messageId}) {
+      return apiHandler.delete(`events/${eventType}/timeline/${messageId}`);
+    },
+    getRulesHistory({
+      eventType,
+      limit = null,
+      offset = null,
+      filter = null,
+      q = null,
+      sort = null,
+      fields = null,
+      expand = null,
+    }) {
+      const params = {limit, offset, filter, q, sort, fields, expand};
+      return apiHandler.getAll(`events/${eventType}/rules/history`, params);
+    },
+    getRulesVersionNumber({eventType, version, fields = null, expand = null}) {
+      const params = {fields, expand};
+      return apiHandler.get(
+        `events/${eventType}/rules/history/${version}`,
+        params
+      );
+    },
+    getRulesVersionDetail({eventType, version, fields = null, expand = null}) {
+      const params = {fields, expand};
+      return apiHandler.get(
+        `events/${eventType}/rules/versions/${version}`,
+        params
+      );
+    },
+    getAllDraftRulesets({
+      eventType,
+      limit = null,
+      offset = null,
+      filter = null,
+      q = null,
+      sort = null,
+      fields = null,
+      expand = null,
+    }) {
+      const params = {limit, offset, filter, q, sort, fields, expand};
+      return apiHandler.getAll(`events/${eventType}/rules/drafts`, params);
+    },
+    createDraftRuleset({eventType, data}) {
+      return apiHandler.post(`events/${eventType}/rules/drafts`, data);
+    },
+    getAllDraftRuleset({eventType, id, fields = null, expand = null}) {
+      const params = {fields, expand};
+      return apiHandler.get(`events/${eventType}/rules/drafts/${id}`, params);
+    },
+    updateDraftRuleset({eventType, id, data}) {
+      return apiHandler.put(`events/${eventType}/rules/drafts/${id}`, data);
+    },
+    deleteDraftRuleset({eventType, id}) {
+      return apiHandler.delete(`events/${eventType}/rules/drafts/${id}`);
+    },
+  };
+}
