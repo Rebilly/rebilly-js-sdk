91e774904e7108ada41db7cc9f21bbd858bd6d9d
Alberto Gualis <agualis@gmail.com>
2021-06-03 14:35:29 +0200

    Add TS types to bundle

--- scripts/merge-types.js
+++ scripts/merge-types.js
@@ -1,14 +1,14 @@
-const { readFileSync, writeFileSync } = require("fs");
+const { readFileSync, writeFileSync } = require('fs');
 
 function merge() {
-    console.log('🧑‍💻  Merging rebilly api types into js-sdk package types');
-    const rebillyApiTypesFilename = './typings/rebilly/index.d.ts';
-    const SDKTypesFilename = './typings/rebilly-js-sdk/index.d.ts';
-    
-    let data1 = readFileSync(rebillyApiTypesFilename, 'utf-8'); 
-    let data2 = readFileSync(SDKTypesFilename, 'utf-8'); 
-    
-    writeFileSync("./dist/rebilly-js-sdk.d.ts", data1 + data2); 
+  console.log('🧑‍💻  Merging rebilly api types into js-sdk package types');
+  const rebillyApiTypesFilename = './typings/rebilly/index.d.ts';
+  const SDKTypesFilename = './typings/rebilly-js-sdk/index.d.ts';
+
+  let data1 = readFileSync(rebillyApiTypesFilename, 'utf-8');
+  let data2 = readFileSync(SDKTypesFilename, 'utf-8');
+
+  writeFileSync('./dist/rebilly-js-sdk.d.ts', data1 + data2);
 }
 
 merge();
