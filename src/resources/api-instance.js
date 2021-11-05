b3b779ba50d9600204d102bde345870197398f16
Rebilly Machine User <devops+github-machineuser@rebilly.com>
2021-11-05 12:06:22 -0400

    update SDK from api-definitions (#348)
    Co-authored-by: cesarlevel <cesarlevel@users.noreply.github.com>

--- src/resources/api-instance.js
+++ src/resources/api-instance.js
@@ -13,6 +13,7 @@ import BroadcastMessagesResource from './broadcast-messages-resource';
 import CheckoutFormsResource from './checkout-forms-resource';
 import CouponsResource from './coupons-resource';
 import CredentialHashesResource from './credential-hashes-resource';
+import CustomDomainsResource from './custom-domains-resource';
 import CustomFieldsResource from './custom-fields-resource';
 import CustomerAuthenticationResource from './customer-authentication-resource';
 import CustomersResource from './customers-resource';
@@ -69,6 +70,7 @@ export class ApiInstance {
     this.checkoutForms = CheckoutFormsResource({apiHandler});
     this.coupons = CouponsResource({apiHandler});
     this.credentialHashes = CredentialHashesResource({apiHandler});
+    this.customDomains = CustomDomainsResource({apiHandler});
     this.customFields = CustomFieldsResource({apiHandler});
     this.customerAuthentication = CustomerAuthenticationResource({apiHandler});
     this.customers = CustomersResource({apiHandler});
