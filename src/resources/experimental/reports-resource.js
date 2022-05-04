c2b1e3ab92a249c36a6c760f438655ecc46a09ca
dasha <darya.baklanova@gmail.com>
2022-05-04 11:06:43 +0300

    feat(reports): support kyc requests summary endpoint

--- src/resources/experimental/reports-resource.js
+++ src/resources/experimental/reports-resource.js
@@ -101,6 +101,14 @@ export default function ReportsResource({apiHandler}) {
             return apiHandler.get(`reports/kyc-rejection-summary`, params);
         },
 
+        getKycRequestSummary({periodStart, periodEnd} = {}) {
+            const params = {
+                periodStart,
+                periodEnd,
+            };
+            return apiHandler.get(`reports/kyc-request-summary`, params);
+        },
+
         getRulesMatchedSummary({eventType, periodStart, periodEnd, limit = null, offset = null, tz = null} = {}) {
             const params = {
                 periodStart,
