428e75525c98b3238f825ec9a363dde7f588452f
Alexander Shkarpetin <ashkarpetin@gmail.com>
2023-03-13 21:57:21 +0600

    Remove credential hashes remove

--- test/unit/mocks/index.js
+++ test/unit/mocks/index.js
@@ -1,7 +1,7 @@
 import customersMock from './customers-mock';
 import invoicesMock from './invoices-mock';
 import gatewayAccountsMock from './gateway-accounts-mock';
-import plaidCredentialsMock from './plaid-credentials-mock';
+import serviceCredentialsMock from './service-credentials-mock';
 import transactionsMock from './transactions-mock';
 
-export default {customersMock, invoicesMock, gatewayAccountsMock, transactionsMock, plaidCredentialsMock};
+export default {customersMock, invoicesMock, gatewayAccountsMock, transactionsMock, serviceCredentialsMock};
--- test/unit/mocks/plaid-credentials-mock.js
+++
@@ -1,4 +0,0 @@
-export default function plaidCredentialsMock({adapter}) {
-    adapter
-        .onPatch('/credential-hashes/plaid/1234').reply(200);
-};
